8eed30a1f5f689776587d1217aa93790f67a9975
xcrash <grishinage@gmail.com>
2011-09-13 02:20:40 +0400

    add properties from wsdl

--- Yandex.Direct/Data Classes/BannerInfo.cs
+++ Yandex.Direct/Data Classes/BannerInfo.cs
@@ -3,24 +3,39 @@ using Newtonsoft.Json;
 
 namespace Yandex.Direct
 {
-    [JsonObject]
-    public class BannerInfo
-    {
-        [JsonProperty("BannerID")]
-        public int BannerId { get; set; }
+	[JsonObject]
+	public class BannerInfo
+	{
+		[JsonProperty("BannerID")]
+		public int BannerId { get; set; }
 
-        [JsonProperty("CampaignID")]
-        public int CampaignId { get; set; }
+		[JsonProperty("CampaignID")]
+		public int CampaignId { get; set; }
 
-        public string Title { get; set; }
-        public string Text { get; set; }
-        public string Href { get; set; }
-        public string Geo { get; set; }
+		public string Title { get; set; }
+		public string Text { get; set; }
+		public string Href { get; set; }
+		public string Geo { get; set; }
 
-        public BannerPhraseInfo[] Phrases { get; set; }
+		public BannerPhraseInfo[] Phrases { get; set; }
 
-        [JsonProperty("Sitelinks")]
-        public List<SiteLinkInfo> SiteLinks { get; set; }
-        public List<string> MinusKeywords { get; set; }
-    }
+		[JsonProperty("Sitelinks")]
+		public List<SiteLinkInfo> SiteLinks { get; set; }
+		public List<string> MinusKeywords { get; set; }
+
+		//imported from wsdl, untested
+		public string Domain { get; set; }
+		//public ContactInfo ContactInfo { get; set; }
+		public string StatusActivating { get; set; }
+		public string StatusArchive { get; set; }
+		public string StatusBannerModerate { get; set; }
+		public string StatusPhrasesModerate { get; set; }
+		public string StatusPhoneModerate { get; set; }
+		public string StatusShow { get; set; }
+		public string IsActive { get; set; }
+		public string StatusSitelinksModerate { get; set; }
+		//public object[] AdWarnings { get; set;}
+		public string FixedOnModeration { get; set; }
+		//public RejectReason[] ModerateRejectionReasons { get; set;}
+	}
 }
--- Yandex.Direct/Data Classes/BannerPhraseInfo.cs
+++ Yandex.Direct/Data Classes/BannerPhraseInfo.cs
@@ -1,41 +1,60 @@
 using System;
-using System.Diagnostics;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace Yandex.Direct
 {
-    [JsonObject]
-    public class BannerPhraseInfo
-    {
-        [JsonProperty("BannerID")]
-        public int BannerId { get; set; }
+	[JsonObject]
+	public class BannerPhraseInfo
+	{
+		[JsonProperty("BannerID")]
+		public int BannerId { get; set; }
 
-        [JsonProperty("CampaignID")]
-        public int CampaignId { get; set; }
+		[JsonProperty("CampaignID")]
+		public int CampaignId { get; set; }
 
-        [JsonProperty("PhraseID")]
-        public int PhraseId { get; set; }
+		[JsonProperty("PhraseID")]
+		public int PhraseId { get; set; }
 
-        public string Phrase { get; set; }
+		public string Phrase { get; set; }
 
-        public decimal? Price { get; set; }
-        public decimal? ContextPrice { get; set; }
+		public decimal? Price { get; set; }
+		public decimal? ContextPrice { get; set; }
 
-        public AutoBudgetPriorityType? AutoBudgetPriority { get; set; }
+		public AutoBudgetPriorityType? AutoBudgetPriority { get; set; }
 
-    	public string IsRubric { get; set; }
+		[JsonConverter(typeof (StringEnumConverter))]
+		public enum AutoBudgetPriorityType
+		{
+			[EnumMember(Value = "Low")] Low,
+			[EnumMember(Value = "Medium")] Medium,
+			[EnumMember(Value = "High")] High,
+		}
 
-        [JsonConverter(typeof(StringEnumConverter))]
-        public enum AutoBudgetPriorityType
-        {
-            [EnumMember(Value = "Low")]
-            Low,
-            [EnumMember(Value = "Medium")]
-            Medium,
-            [EnumMember(Value = "High")]
-            High,
-        }
-    }
+		//imported from wsdl, untested
+		public string IsRubric { get; set; }
+		public int? Clicks { get; set; }
+		public int? FirstPlaceClicks { get; set; }
+		public int? PremiumClicks { get; set; }
+		public int? Shows { get; set; }
+		public float? Min { get; set; }
+		public float? Max { get; set; }
+		public float? PremiumMin { get; set; }
+		public float? PremiumMax { get; set; }
+		public float? CTR { get; set; }
+		public float? FirstPlaceCTR { get; set; }
+		public float? PremiumCTR { get; set; }
+		public string LowCTRWarning { get; set; }
+		public string LowCTR { get; set; }
+		//public CoverageInfo[] ContextCoverage { get; set; }
+		public float[] Prices { get; set; }
+		//public CoverageInfo[] Coverage { get; set; }
+		public string AutoBroker { get; set; }
+		public float? CurrentOnSearch { get; set; }
+		public float? MinPrice { get; set; }
+		public string StatusPhraseModerate { get; set; }
+		public string ContextLowCTR { get; set; }
+		//public PhraseUserParams UserParams { get; set; }
+	}
 }
